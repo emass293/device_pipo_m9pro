957708d3715b13e2fd21c5b900c7c9e254793607
Eric <eric29950@gmail.com>
2015-06-05 20:13:10 +0200

    initial version

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,31 +1,112 @@
-USE_CAMERA_STUB := true
+# BoardConfig.mk
+#
+# Product-specific compile-time definitions.
+#
+DEVICE_FOLDER := device/pipo/m9pro
 
-# inherit from the proprietary version
--include vendor/pipo/m9pro/BoardConfigVendor.mk
+# The generic product target doesn't have any hardware-specific pieces.
+TARGET_NO_BOOTLOADER := true
+#TARGET_NO_KERNEL := true
+
+PRODUCT_CHARACTERISTICS := tablet
 
 TARGET_ARCH := arm
-TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_ARCH_VARIANT_FPU := neon
+ARCH_ARM_HAVE_NEON := true
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a7
 TARGET_CPU_SMP := true
+TARGET_CPU_VARIANT := cortex-a9
 ARCH_ARM_HAVE_TLS_REGISTER := true
+ARCH_ARM_HAVE_VFP := true
+ARCH_ARM_HAVE_NEON := true
+ARCH_ARM_HAVE_ARMV7A := true
+TARGET_BOARD_PLATFORM := rk3188
+TARGET_BOARD_HARDWARE := rk30board
 
-TARGET_BOOTLOADER_BOARD_NAME := m9pro
+TARGET_EXTRA_CFLAGS += $(call cc-option,-mtune=cortex-a9,$(call cc-option,-mtune=cortex-a8)) $(call cc-option,-mcpu=cortex-a9,$(call cc-option,-mcpu=cortex-a8))
+#TARGET_GLOBAL_CFLAGS += -mtune=cortex-a9 -mcpu=cortex-a9 -mfpu=neon -mfloat-abi=softfp
+#TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a9 -mcpu=cortex-a9 -mfpu=neon -mfloat-abi=softfp
 
-BOARD_KERNEL_CMDLINE := 
+BOARD_KERNEL_CMDLINE := console=ttyFIQ0 androidboot.console=ttyFIQ0 init=/init initrd=0x62000000,0x00800000 mtdparts=rk29xxnand:0x00002000@0x00002000(misc),0x00006000@0x00004000(kernel),0x00006000@0x0000a000(boot),0x00010000@0x00010000(recovery),0x00020000@0x00020000(backup),0x00040000@0x00040000(cache),0x01000000@0x00080000(userdata),0x00003000@0x01080000(metadata),0x00003000@0x01083000(kpanic),0x00180000@0x01086000(system),-@0x01206000(user)
+TARGET_PREBUILT_KERNEL := $(DEVICE_FOLDER)/kernel
+
+# TWRP
+TARGET_PREBUILT_RECOVERY_KERNEL := $(DEVICE_FOLDER)/kernel
+TARGET_RECOVERY_FSTAB := $(DEVICE_FOLDER)/recovery/recovery.fstab
+BOARD_HAS_LARGE_FILESYSTEM := true
+DEVICE_RESOLUTION := 1920x1200
+BOARD_USE_CUSTOM_RECOVERY_FONT:= \"roboto_15x24.h\"
+TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
+TW_BOARD_CUSTOM_GRAPHICS := ../../../$(DEVICE_FOLDER)/recovery/graphics.c
+TW_DEFAULT_EXTERNAL_STORAGE := true
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TWHAVE_SELINUX := true
+TW_INCLUDE_FB2PNG := true
+
+
+# Parameters
+BOARD_RAMDISK_BASE := 0x62000000
 BOARD_KERNEL_BASE := 0x60400000
-BOARD_KERNEL_PAGESIZE := 16384
+BOARD_KERNEL_PAGESIZE := 4096
 
 # fix this up by examining /proc/mtd on a running device
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_BOOTIMAGE_PARTITION_SIZE := 12582912
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 805306368
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 8589934592
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-TARGET_PREBUILT_KERNEL := device/pipo/m9pro/kernel
+# Set up the flash file formats so that we can flash them directly
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true
+BOARD_SYSTEM_FILESYSTEM    := ext4
+BOARD_HAS_LARGE_FILESYSTEM := true
 
-BOARD_HAS_NO_SELECT_BUTTON := true
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := false
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_FOLDER)/bluetooth
+#BOARD_BLUEDROID_VENDOR_CONF := $(DEVICE_FOLDER)/bluetooth/vnd_rockchip.txt
+
+# Wifi
+BOARD_HAVE_WIFI := true
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE := bcmdhd
+WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA := "/system/etc/firmware/fw_bcm4329.bin"
+WIFI_DRIVER_FW_PATH_P2P := "/system/etc/firmware/fw_bcm4329_p2p.bin"
+WIFI_DRIVER_FW_PATH_AP := "/system/etc/firmware/fw_bcm4329_apsta.bin"
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/rkwifi.ko"
+WIFI_DRIVER_MODULE_NAME := "wlan"
+
+# Graphics
+BOARD_EGL_CFG := $(DEVICE_FOLDER)/egl.cfg
+USE_OPENGL_RENDERER := true
+BOARD_USES_HGL := true
+BOARD_USES_OVERLAY := true
+TARGET_USES_ION := true
+
+# Audio
+BOARD_USES_GENERIC_AUDIO := flase
+BOARD_USES_ALSA_AUDIO := true
+
+# Camera Setup
+USE_CAMERA_STUB := true
+
+# No HWCOMPOSER - TO DO
+BOARD_USES_HWCOMPOSER := true
+
+# Enable WEBGL in WebKit
+ENABLE_WEBGL := true
+
+#GPS
+BOARD_HAVE_GPS := true
+
--- device_m9pro.mk
+++ device_m9pro.mk
@@ -1,13 +1,11 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+TARGET_BUILD_TYPE := release #debug
 
-# The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
+# default is nosdcard, S/W button enabled in resource
+DEVICE_FOLDER := device/pipo/m9pro
+DEVICE_PACKAGE_OVERLAYS := $(DEVICE_FOLDER)/overlay
+PRODUCT_CHARACTERISTICS := tablet
 
-$(call inherit-product-if-exists, vendor/pipo/m9pro/m9pro-vendor.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/pipo/m9pro/overlay
-
-LOCAL_PATH := device/pipo/m9pro
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
 else
@@ -17,8 +15,213 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
-$(call inherit-product, build/target/product/full.mk)
+#Pour boot.img
+PRODUCT_COPY_FILES += \
+	$(call find-copy-subdir-files,*,$(DEVICE_FOLDER)/ramdisk,root) \
+	$(call find-copy-subdir-files,*,$(DEVICE_FOLDER)/ramdisk/res/images/charger/,root/res/images/charger)
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := full_m9pro
-PRODUCT_DEVICE := m9pro
+#Pour system.img
+
+# Copy app
+PRODUCT_COPY_FILES += \
+	$(call find-copy-subdir-files,*,$(DEVICE_FOLDER)/prebuilt/app,system/app)
+
+# Copy bin
+PRODUCT_COPY_FILES += \
+	$(call find-copy-subdir-files,*,$(DEVICE_FOLDER)/prebuilt/bin,system/bin)
+
+# Copy etc
+PRODUCT_COPY_FILES += \
+	$(call find-copy-subdir-files,*,$(DEVICE_FOLDER)/prebuilt/etc,system/etc)
+
+# Copy etc bluetooth
+#PRODUCT_COPY_FILES += \
+#	$(call find-copy-subdir-files,*,$(DEVICE_FOLDER)/prebuilt/etc/bluetooth,system/etc/bluetooth)
+
+# Copy etc init.d scripts
+PRODUCT_COPY_FILES += \
+	$(call find-copy-subdir-files,*,$(DEVICE_FOLDER)/prebuilt/etc/init.d,system/etc/init.d)
+
+# Copy etc ppp
+PRODUCT_COPY_FILES += \
+	$(call find-copy-subdir-files,*,$(DEVICE_FOLDER)/prebuilt/etc/ppp,system/etc/ppp)
+
+# Copy etc usb_modeswitch.d 
+PRODUCT_COPY_FILES += \
+	$(call find-copy-subdir-files,*,$(DEVICE_FOLDER)/prebuilt/etc/usb_modeswitch.d,system/etc/usb_modeswitch.d)
+
+# Copy etc firmware
+PRODUCT_COPY_FILES += \
+	$(call find-copy-subdir-files,*,$(DEVICE_FOLDER)/prebuilt/etc/firmware,system/etc/firmware)
+
+# Copy etc gps
+PRODUCT_COPY_FILES += \
+	$(call find-copy-subdir-files,*,$(DEVICE_FOLDER)/prebuilt/etc/gps,system/etc/gps)
+
+# Copy etc wifi
+#PRODUCT_COPY_FILES += \
+#	$(call find-copy-subdir-files,*,$(DEVICE_FOLDER)/prebuilt/etc/wifi,system/etc/wifi)
+
+# Copy lib hw
+PRODUCT_COPY_FILES += \
+	$(call find-copy-subdir-files,*,$(DEVICE_FOLDER)/prebuilt/lib/hw,system/lib/hw)
+
+# Copy lib
+PRODUCT_COPY_FILES += \
+	$(call find-copy-subdir-files,*,$(DEVICE_FOLDER)/prebuilt/lib,system/lib)
+
+# Copy lib egl
+PRODUCT_COPY_FILES += \
+	$(call find-copy-subdir-files,*,$(DEVICE_FOLDER)/prebuilt/lib/egl,system/lib/egl)
+
+# Copy modules
+PRODUCT_COPY_FILES += \
+	$(call find-copy-subdir-files,*,$(DEVICE_FOLDER)/prebuilt/lib/modules,system/lib/modules)
+
+# Copy media
+#PRODUCT_COPY_FILES += \
+#	$(call find-copy-subdir-files,*,$(DEVICE_FOLDER)/prebuilt/media,system/media)
+
+# Copy framework
+PRODUCT_COPY_FILES += \
+	$(call find-copy-subdir-files,*,$(DEVICE_FOLDER)/prebuilt/framework,system/framework)
+
+# Copy preinstall
+PRODUCT_COPY_FILES += \
+	$(call find-copy-subdir-files,*,$(DEVICE_FOLDER)/prebuilt/preinstall,system/preinstall)
+
+# Copy vendor firmware
+PRODUCT_COPY_FILES += \
+	$(call find-copy-subdir-files,*,$(DEVICE_FOLDER)/prebuilt/vendor/firmware,system/vendor/firmware)
+
+# Copy vendor lib
+PRODUCT_COPY_FILES += \
+	$(call find-copy-subdir-files,*,$(DEVICE_FOLDER)/prebuilt/vendor/lib,system/vendor/lib)
+
+# Copy keylayouts
+#PRODUCT_COPY_FILES += \
+#	$(call find-copy-subdir-files,*,$(DEVICE_FOLDER)/prebuilt/usr/keylayout,system/usr/keylayout)
+
+# hardware-specific feature permissions
+PRODUCT_COPY_FILES += \
+        $(call find-copy-subdir-files,*,$(DEVICE_FOLDER)/prebuilt/etc/permissions,system/etc/permissions)
+
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+        service.adb.root=1 \
+        ro.secure=0 \
+        ro.allow.mock.location=1 \
+        ro.debuggable=1 \
+        persist.sys.usb.config=mtp \
+        ro.rk.systembar.voiceicon=true
+
+# enable ADB
+PRODUCT_PROPERTY_OVERRIDES := \
+        service.adb.root=1 \
+        ro.secure=0 \
+        ro.allow.mock.location=1 \
+        ro.debuggable=1
+
+# Drm
+PRODUCT_PACKAGES += \
+        drmservice
+
+# Camera
+PRODUCT_PACKAGES += \
+        Camera \
+        camera.rk30board
+
+# Audio
+PRODUCT_PACKAGES += \
+        audio.primary.default \
+        audio.primary.rk30board \
+        audio_policy.default \
+        audio_policy.rk30board \
+        alsa.default \
+        audio.alsa_usb.rk30board \
+        acoustics.default \
+        audio.r_submix.default \
+        tinyplay \
+        tinycap \
+        tinymix \
+        audio.a2dp.default \
+        audio.usb.default \
+        libtinyalsa \
+        libaudioutils
+
+# Hal modules
+PRODUCT_PACKAGES += \
+	lights.rk30board \
+   	power.rk3188 \
+        sensors.rk30board \
+        hwcomposer.rk30board \
+        gralloc.rk30board \
+        gpu.rk30board 
+
+
+PRODUCT_PACKAGES += \
+        librs_jni \
+        com.android.future.usb.accessory
+
+PRODUCT_PACKAGES += \
+        com.broadcom.bt \
+        com.broadcom.bt.xml \
+        libbt-client-api
+
+
+# VPU Libs
+PRODUCT_PACKAGES += \
+	libapedec \
+	libjpeghwdec \
+	libjpeghwenc \
+        libjesancache \
+        libffmpeg_on2 \
+        libffmpeg \
+        libhevcdec \
+	libOMX_Core \
+	libomxvpu_dec \
+	libomxvpu_enc \
+	librk_demux \
+        libstagefrighthw \
+        libRkOMX_Resourcemanager \
+	librkwmapro \
+	libffmpeg \
+	librkffplayer \
+  	librk_on2 \
+        librkswscale \
+        libffmpegvpu \
+        libvpu
+
+# filesystem tools
+PRODUCT_PACKAGES += \
+        make_ext4fs \
+        setup_fs \
+        static_busybox \
+        utility_make_ext4fs \
+        libstagefrighthw
+
+# French
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.product.locale.language=fr \
+        ro.product.locale.region=FR
+
+# Fix for dalvik-cache
+PRODUCT_PROPERTY_OVERRIDES += \
+	dalvik.vm.dexopt-data-only=1
+
+# GPS
+PRODUCT_PACKAGES += \
+        libloc_adapter \
+        libloc_eng \
+        libgps.utils \
+        gps.rk30board
+
+# 3G
+PRODUCT_PACKAGES += \
+        rild \
+        chat
+
+# android core stuff
+$(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
+$(call inherit-product, build/target/product/full_base.mk)
+
